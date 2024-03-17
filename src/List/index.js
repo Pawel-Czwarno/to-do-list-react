789377eb1502694dbf8f96a02348435212cecfd5
Paweł Czwarno <pawel.czwarno@gmail.com>
2024-03-17 08:03:31 +0100

    Formatted code in List module

--- src/List/index.js
+++ src/List/index.js
@@ -3,12 +3,15 @@ import "./style.css";
 const List = (props) => (
    <ul className="list">
       {props.tasks.map(task => (
-         <li className={`list__element${task.done && props.hideDoneTasks ? " list__element--hidden" : ""}`} key={task.id}>
-            <button className="list__button list__button--done">{task.done ? "✓" : ""}
+         <li className={`list__element${task.done && props.hideDoneTasks ? " list__element--hidden" : ""}`}
+            key={task.id}>
+            <button className="list__button list__button--done">
+               {task.done ? "✓" : ""}
             </button>
             <div className={`${task.done ? "list__element--done" : ""}`}>
                {task.content}</div>
-            <button className="list__button list__button--remove">🗑
+            <button className="list__button list__button--remove">
+               🗑
             </button>
          </li>
       ))}
